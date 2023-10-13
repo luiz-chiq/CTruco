7fd5d124454b8ffa239c7db60b2aea81698927dc
Laura Luchiari <95256189+lauraluch@users.noreply.github.com>
2023-10-12 21:31:21 -0300

    feat: create function to get the highest card

--- bot-impl/src/main/java/com/casal/impl/vapoBot/VapoBot.java
+++ bot-impl/src/main/java/com/casal/impl/vapoBot/VapoBot.java
@@ -22,6 +22,23 @@ public class VapoBot implements BotServiceProvider {
 
     @Override
     public CardToPlay chooseCard(GameIntel intel) {
+        System.out.println("---------------");
+        System.out.println("getCards: " + intel.getCards());
+        System.out.println("getOpenCards: " + intel.getOpenCards());
+        System.out.println("getVira: " + intel.getVira());
+        System.out.println("getHandPoints: " + intel.getHandPoints());
+        System.out.println("getOpponentScore: " + intel.getOpponentScore());
+        System.out.println("getScore: " + intel.getScore());
+        System.out.println("getOpponentCard: " + intel.getOpponentCard());
+        System.out.println("getRoundResults: " + intel.getRoundResults());
+
+        System.out.println(" average (?): " + getAverageCardValue(intel));
+
+        if (intel.getOpponentCard().isPresent()) {
+            TrucoCard opponentCard = intel.getOpponentCard().get();
+        }
+
+        System.out.println("your highest card: " + getYourHighestCard(intel));
         return CardToPlay.of(intel.getCards().get(0));
     }
 
@@ -41,5 +58,17 @@ public class VapoBot implements BotServiceProvider {
         }
         double average = (double) values/intel.getCards().size();
         return average;
+    }
+
+    private TrucoCard getYourHighestCard(GameIntel intel){
+        TrucoCard highestCard = intel.getCards().get(0);
+
+        for (TrucoCard card : intel.getCards()) {
+            if (card.relativeValue(intel.getVira()) > highestCard.relativeValue(intel.getVira())) {
+                highestCard = card;
+            }
+        }
+
+        return highestCard;
     }
 }
