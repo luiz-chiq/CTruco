489d85d2638ab34c226678e7ba730df956181d09
Laura Luchiari <95256189+lauraluch@users.noreply.github.com>
2023-10-15 16:38:23 -0300

    refactor: adds a test to the test class

--- bot-impl/src/main/java/com/casal/impl/vapoBot/VapoBot.java
+++ bot-impl/src/main/java/com/casal/impl/vapoBot/VapoBot.java
@@ -5,6 +5,8 @@ import com.bueno.spi.model.GameIntel;
 import com.bueno.spi.model.TrucoCard;
 import com.bueno.spi.service.BotServiceProvider;
 
+import java.util.List;
+
 public class VapoBot implements BotServiceProvider {
 
     @Override
@@ -14,7 +16,13 @@ public class VapoBot implements BotServiceProvider {
 
     @Override
     public boolean decideIfRaises(GameIntel intel) {
-        if (getAverageCardValue(intel) > 7) {
+        // Se possuir uma média de cartas maiores que o valor 8, pedir truco:
+//        System.out.println(getAverageCardValue(intel));
+        if (getAverageCardValue(intel) > 8) {
+            return true;
+        }
+        // Se o jogador até tiver feito, mas tiver feito com cartas baixas, pedir truco:
+        else if (checkHowManyOpponentCardsAreBad(intel) > 1) {
             return true;
         }
         return false;
@@ -22,8 +30,8 @@ public class VapoBot implements BotServiceProvider {
 
     @Override
     public CardToPlay chooseCard(GameIntel intel) {
-        System.out.println("---------------");
-        System.out.println("getCards: " + intel.getCards());
+//        System.out.println("---------------");
+//        System.out.println("getCards: " + intel.getCards());
 //        System.out.println("getOpenCards: " + intel.getOpenCards());
 //        System.out.println("getVira: " + intel.getVira());
 //        System.out.println("getHandPoints: " + intel.getHandPoints());
@@ -34,21 +42,40 @@ public class VapoBot implements BotServiceProvider {
 //
 //        System.out.println(" average (?): " + getAverageCardValue(intel));
 
-        if (intel.getOpponentCard().isPresent()) {
-            TrucoCard opponentCard = intel.getOpponentCard().get();
+//        if (intel.getOpponentCard().isPresent()) {
+//            TrucoCard opponentCard = intel.getOpponentCard().get();
+//            System.out.println(intel.getHandPoints());
+//            if (intel.getHandPoints() == 0) {
+//                System.out.println("vai jogar a maior carta");
+//                return CardToPlay.of(getYourHighestCard(intel));
+//            }
+//
+//        }
+//        System.out.println(checkHowManyOpponentCardsAreBad(intel));
+//        System.out.println("your highest card: " + getYourHighestCard(intel));
+//        System.out.println("your lowest card: " + getYourLowestCard(intel));
+
+//        System.out.println("handPoints: " + intel.getHandPoints());
+//        System.out.println("roundResults: " + intel.getRoundResults());
+
+        // Se ele possui uma derrota ou se não venceu ainda a primeira, tenta vencer:
+        if (intel.getRoundResults().contains(GameIntel.RoundResult.LOST) || intel.getRoundResults().isEmpty()) {
+//            System.out.println("vai jogar a maior carta");
+            return CardToPlay.of(getYourHighestCard(intel));
         }
 
-        System.out.println("your highest card: " + getYourHighestCard(intel));
-        System.out.println("your lowest card: " + getYourLowestCard(intel));
-        return CardToPlay.of(intel.getCards().get(0));
+
+
+//        System.out.println("vai jogar a primeira carta");
+        return CardToPlay.of(getYourLowestCard(intel));
     }
 
     @Override
     public int getRaiseResponse(GameIntel intel) {
         double average = getAverageCardValue(intel);
-        if (average < 4 || average > 9) return 0;
-        else if (average > 7) return 1;
-        return -1;
+      if (average < 4 || average > 8) return 1;
+      else if (average > 6) return 0;
+      return -1;
     }
 
     private double getAverageCardValue(GameIntel intel){
@@ -84,4 +111,17 @@ public class VapoBot implements BotServiceProvider {
 
         return lowestCard;
     }
+
+    public int checkHowManyOpponentCardsAreBad(GameIntel intel) {
+        int badCardsCounter = 0;
+        if (intel.getOpponentCard().isPresent()) {
+            TrucoCard opponentCard = intel.getOpponentCard().get();
+            if (opponentCard.relativeValue(intel.getVira()) < 8) {
+                badCardsCounter++;
+            }
+
+        }
+        return badCardsCounter;
+    }
+
 }
--- bot-impl/src/test/java/com/casal/impl/vapoBot/VapoBotTest.java
+++ bot-impl/src/test/java/com/casal/impl/vapoBot/VapoBotTest.java
@@ -1,18 +1,66 @@
 package com.casal.impl.vapoBot;
 
+import com.bueno.spi.model.CardRank;
+import com.bueno.spi.model.CardSuit;
+import com.bueno.spi.model.GameIntel;
+import com.bueno.spi.model.TrucoCard;
+import com.indi.impl.addthenewsoul.AddTheNewSoul;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 class VapoBotTest {
 
+    private GameIntel.StepBuilder stepBuilder;
+    private List<GameIntel.RoundResult> results;
+    private List<TrucoCard> openCards;
+    private List<TrucoCard> botCards;
+    private TrucoCard vira;
+    private VapoBot vapoBot;
+
+    @BeforeEach
+    public void setUp(){
+        vapoBot = new VapoBot();
+    }
+
     @Test
     void getYourHighestCard() {
-        
+
     }
 
     @Test
     void getYourLowestCard() {
 
+    }
+
+    @Test
+    @DisplayName("Should increase if sum of card values is above average")
+    public void shouldIncreaseIfSumOfCardValuesIsAboveAverageTest(){
+        TrucoCard vira = TrucoCard.of(CardRank.SIX, CardSuit.DIAMONDS);
+
+        List<TrucoCard> openCards = Arrays.asList(
+                TrucoCard.of(CardRank.SIX, CardSuit.DIAMONDS),
+                TrucoCard.of(CardRank.FIVE, CardSuit.DIAMONDS));
+        List<TrucoCard> cards = Arrays.asList(
+                TrucoCard.of(CardRank.THREE, CardSuit.CLUBS),
+                TrucoCard.of(CardRank.FOUR, CardSuit.HEARTS),
+                TrucoCard.of(CardRank.SEVEN, CardSuit.CLUBS));
+
+        TrucoCard opponentCard = TrucoCard.of(CardRank.FIVE, CardSuit.DIAMONDS);
+
+        stepBuilder = GameIntel.StepBuilder.with()
+                .gameInfo(List.of(GameIntel.RoundResult.LOST), openCards, vira, 1)
+                .botInfo(cards, 0)
+                .opponentScore(0)
+                .opponentCard(opponentCard);
+        assertTrue(vapoBot.decideIfRaises(stepBuilder.build()));
     }
 }
